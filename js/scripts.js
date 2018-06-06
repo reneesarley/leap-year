78cffacf8bbb46aa3a1ff16d1132e7c74ccc6815
ReneeSarley <reneesarley@gmail.com>
2018-06-05 20:50:01 -0700

    pass not divisable by 100 spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
 $(function(){
-alert("this is working");
 
+  var leapYear = function(year) {
+  if(year % 100 === 0){
+    return false
+  } else if(year % 4 === 0){
+    return true
+  } else{
+    return false;
+  }
+};
+
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+
+  });
 });

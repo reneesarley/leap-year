bd5e170433b05a1519f9d90ff4babbde882083ae
ReneeSarley <reneesarley@gmail.com>
2018-06-05 20:55:09 -0700

    pass divisable by 400 spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,9 @@
 $(function(){
 
   var leapYear = function(year) {
-  if(year % 100 === 0){
-    return false
-  } else if(year % 4 === 0){
+  if((year % 100 !== 0) && (year % 4 === 0) || (year % 400 === 0)){
     return true
-  } else{
+  }  else{
     return false;
   }
 };
